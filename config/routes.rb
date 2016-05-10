e6f3d3f68a51fd4fdf6b38126254b6a76b6825e9
Drew Johnson <drew.johnson.dev@gmail.com>
2016-05-10 09:53:10 +0100

    Add dummy home and help views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'dummy#hiya'
 
   # Example of regular route:
