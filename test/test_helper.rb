f2462942161d66154a3aad41f5bca275f5e4cfea
Drew Johnson <drew.johnson.dev@gmail.com>
2016-06-04 22:03:44 +0100

    Add user model w/password functionality

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ require 'rails/test_help'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 
+ActiveRecord::Migration.maintain_test_schema!
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
